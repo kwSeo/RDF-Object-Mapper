b68e9ba73000fc1a0798415229764b78a1ae877c
kwSeo <tkruddnjs1@naver.com>
2015-07-04 23:43:13 +0900

    testing JUnit and sample mapper(XML, DTD) file

---
+++ test/java/team/afgm/rdfom/test/TestClass.java
@@ -0,0 +1,17 @@
+package team.afgm.rdfom.test;
+
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestClass {
+	@Test
+	public void testMethod(){
+		System.out.println("Start Project with Github!!!");
+	}
+	
+	@BeforeClass
+	public static void init(){
+		System.out.println("wait...");
+	}
+}
